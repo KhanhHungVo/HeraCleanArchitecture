b67770a0f9884a794315b872de52a9485eb5dde5
Hung Vo <khanhhungvo@gmail.com>
2022-06-25 10:21:23 +0700

    update docker for arm

--- src/Hera.Application/Users/Services/CryptoService.cs
+++ src/Hera.Application/Users/Services/CryptoService.cs
@@ -8,14 +8,10 @@ namespace Hera.Application.Users.Services
     public class CryptoService : ICryptoService
     {
         private readonly IHeraDbContext _context;
-        private readonly IMapper _mapper;
-        private readonly IConfiguration _config;
 
-        public CryptoService(IMapper mapper, IHeraDbContext context, IConfiguration config)
+        public CryptoService(IHeraDbContext context)
         {
-            _mapper = mapper;
             _context = context;
-            _config = config;
         }
 
         public async Task RecordCryptoCoins(List<CryptoCoin> cryptoCoins)
